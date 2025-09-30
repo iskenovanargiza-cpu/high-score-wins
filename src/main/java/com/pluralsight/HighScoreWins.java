890d0aaf0675a7ab6724d78f3119a46a9ef4b415
novyjpolzovatel <iskenova.nargiza@gmail.com>
2025-09-29 21:23:20 -0400

    First try, code has error.

---
+++ src/main/java/com/pluralsight/HighScoreWins.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class HighScoreWins {
+    public static void main(String[] args) {
+        Scanner format1 = new Scanner(System.in);
+        System.out.println("Please enter a game score:\n");
+        String input = format1.nextLine().trim();
+        String[] formatted = input.split("\\|");
+        String teamNames = formatted[0];
+        String scores = formatted[1];
+
+        String[] splitTeams = teamNames.split("\\:");
+        String[] splitScores = scores.split("\\:");
+
+        String team1 = splitTeams[0];
+        String team2 = splitTeams[1];
+
+        int score1 = Integer.parseInt(splitScores[2]);
+        int score2 = Integer.parseInt(splitScores[3]);
+
+        if (score1 > score2) {
+            System.out.printf("Winner: %s ", team1);
+        } else if (score2 > score1) {
+            System.out.printf("Winner: %s ", team2);
+        } else
+            System.out.println("Scores are equal");
+
+    }
+}
